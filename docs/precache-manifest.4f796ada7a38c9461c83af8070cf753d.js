a53145d2009c77319f73ec68e210c3698bb41a7e
Felipe Muñoz <darkspy1976@gmail.com>
2020-08-03 19:52:26 -0400

    docs agregado

---
+++ docs/precache-manifest.4f796ada7a38c9461c83af8070cf753d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78a57279abce1d00f2d6237f6c9f158",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25c9110de1b18274ab3d",
+    "url": "/static/css/main.26d25779.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25c9110de1b18274ab3d",
+    "url": "/static/js/main.a8571da0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
